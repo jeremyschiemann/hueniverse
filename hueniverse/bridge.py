cae53ecb71a3763fa9bba9b3e9a75354528f7bf1
Jeremy Schiemann <jeremy.schiemann@googlemail.com>
2024-02-27 18:53:59 +0100

    handle self signed certs and verify CN

--- hueniverse/bridge.py
+++ hueniverse/bridge.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
-from typing import List
-
+import ssl
+from typing import List, Dict, Any
+from cryptography.x509 import load_pem_x509_certificate
+import certifi
 import httpx
 
 from hueniverse.hue_types import DiscoveredBridge, ResourceType, AppKeyResponse, Light
 
 _DISCOVERY_URL = 'https://discovery.meethue.com/'
+
 
 class AsyncBridge:
     """Represents a Philips Hue bridge."""
@@ -16,8 +19,7 @@ class AsyncBridge:
         ip_address: str,
         app_key: str | None = None,
         id: str | None = None,
-        port: int | None = None,
-        http_client: httpx.AsyncClient | None = None,
+        port: int = 443,
     ) -> None:
         """Initialize a Bridge instance."""
         self._id = id
@@ -25,10 +27,23 @@ class AsyncBridge:
         self._app_key = app_key
         self._port = port
 
-        self._url = f'https://{self.ip_address}/clip/v2//resource'
-        self._key_post_url = f'https://{self.ip_address}/api'
+        self._url = f'https://{self.ip_address}/clip/v2/resource'
 
-        self._client = http_client or httpx.AsyncClient()
+        self._uses_self_signed_cert = False
+
+        if port and ip_address:
+            server_cert = ssl.get_server_certificate((ip_address, port)).encode('utf-8')
+            cert = load_pem_x509_certificate(server_cert)
+            if cert.issuer == cert.subject:
+                self._uses_self_signed_cert = True
+            subject_cn = [x.rfc4514_string() for x in cert.subject.rdns if x.rfc4514_string().startswith('CN')][0]
+            if not subject_cn.endswith(id):
+                raise AssertionError('Bridge ID does not match certificate CN.')
+
+        self._client = httpx.AsyncClient(
+            verify=not self._uses_self_signed_cert,
+            #cert='./hueniverse/signify.pem',
+        )
 
 
     @property
@@ -51,7 +66,13 @@ class AsyncBridge:
         """Close the HTTP client."""
         await self._client.aclose()
 
-    async def _get(self, resource_type: ResourceType, resource_identifier: str | None = None):
+
+    async def _get_resource(
+        self,
+        resource_type:
+        ResourceType,
+        resource_identifier: str | None = None,
+    ) -> List[Dict[str, Any]]:
 
         assert self._app_key is not None, 'App key is required for this operation'
 
@@ -67,27 +88,32 @@ class AsyncBridge:
         )
 
         response.raise_for_status()
-        return response.json()
+
+        json_response = response.json()
+
+        if json_response['errors']:
+            raise ConnectionError(json_response['errors'])
+
+        return json_response['data']
 
     async def get_lights(self) -> List[Light]:
         """Get a list of lights connected to the bridge."""
 
-        res = await self._get(ResourceType.LIGHT)
-        return [Light(**x) for x in res['data']]
+        return [Light(**x) for x in await self._get_resource(ResourceType.LIGHT)]
 
 
-    async def get_light_by_id(self, resource_identifier: str) -> Light | None:
+    async def get_light_by_id(self, resource_identifier: str) -> Light:
         """Get a light by its resource identifier."""
 
-        res = await self._get(ResourceType.LIGHT, resource_identifier)
-        return Light(**res['data'][0]) if res['data'] else None
+        res = await self._get_resource(ResourceType.LIGHT, resource_identifier)
+        return Light(**res[0])
 
 
     async def create_app_key(self, app_name: str, instance_name: str) -> AppKeyResponse:
         """Create app key, for the app to communicate with the bridge"""
 
         response = await self._client.post(
-            url=self._key_post_url,
+            url=f'https://{self.ip_address}/api',
             json={
                 'devicetype': f'{app_name}#{instance_name}',
                 'generateclientkey': True,
@@ -103,17 +129,17 @@ class AsyncBridge:
 
 
     @classmethod
-    async def from_discover(cls, http_client: httpx.AsyncClient) -> List[AsyncBridge]:
+    def from_discover(cls) -> List[AsyncBridge]:
         """Create Bridge instances from discovered bridges."""
         return [
-            cls(id=bridge.id, ip_address=bridge.internalipaddress, port=bridge.port, http_client=http_client)
-            for bridge in await cls.discover(http_client)
+            cls(id=bridge.id, ip_address=bridge.internalipaddress, port=bridge.port)
+            for bridge in cls.discover()
         ]
 
     @classmethod
-    async def from_discover_single(cls, http_client: httpx.AsyncClient) -> AsyncBridge | None:
+    def from_discover_single(cls) -> AsyncBridge | None:
         """Create a single Bridge instance from the first discovered bridge."""
-        discovered_bridges = await cls.discover(http_client)
+        discovered_bridges = cls.discover()
 
         if not discovered_bridges:
             return None
@@ -122,14 +148,12 @@ class AsyncBridge:
             id=discovered_bridges[0].id,
             ip_address=discovered_bridges[0].internalipaddress,
             port=discovered_bridges[0].port,
-            http_client=http_client,
         )
 
 
     @staticmethod
-    async def discover(http_client: httpx.AsyncClient | None = None) -> List[DiscoveredBridge]:
+    def discover() -> List[DiscoveredBridge]:
         """Discover Philips Hue bridges on the local network."""
-        http = http_client or httpx.AsyncClient()
-        result = await http.get(url=_DISCOVERY_URL)
+        result = httpx.get(url=_DISCOVERY_URL)
         result.raise_for_status()
         return [DiscoveredBridge(**item) for item in result.json()]
